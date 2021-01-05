01634d797bbfdb4ba14973609acd3e9a6e59c62d
aliak <a.akhoundi79@yahoo.com>
2021-01-05 16:41:51 +0330

    users routes created

--- user/tests.py
+++ user/tests.py
@@ -1,28 +1,28 @@
-# from django.test import TestCase
-# from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 
-# class UserAcountTest(TestCase):
+class UserAcountTest(TestCase):
 
-#     def test_new_superuser(self):
-#         db  = get_user_model()
-#         super_user = db.objects.create_superuser(
-#             'testuser@super.com',
-#             'username',
-#             'firstname',
-#             'password'
-#         )
+    def test_new_superuser(self):
+        db  = get_user_model()
+        super_user = db.objects.create_superuser(
+            'testuser@super.com',
+            'username',
+            'firstname',
+            'password'
+        )
 
-#         self.assertEqual(super_user.email , 'testuser@super.com')
-#         self.assertTrue(super_user.is_superuser)
-#         self.assertTrue(super_user.is_active)
-#         self.assertTrue(super_user.is_staff)
+        self.assertEqual(super_user.email , 'testuser@super.com')
+        self.assertTrue(super_user.is_superuser)
+        self.assertTrue(super_user.is_active)
+        self.assertTrue(super_user.is_staff)
 
-#         with self.assertRaises(ValueError):
-#             db.objects.create_superuser(
-#                 email='testuser1@super.com',
-#                 user_name='username1',
-#                 first_name='firstname1',
-#                 password='password1',
-#                 is_superuser = False
-#             )
+        with self.assertRaises(ValueError):
+            db.objects.create_superuser(
+                email='testuser1@super.com',
+                user_name='username1',
+                first_name='firstname1',
+                password='password1',
+                is_superuser = False
+            )
--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
+from .views import (
+    UserViewSet
+)
 
-urlpatterns = [
-]
+
+router = SimpleRouter()
+router.register(r'users' ,UserViewSet )
+
+
+urlpatterns = router.urls
