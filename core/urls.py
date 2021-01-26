11378c6d62290a2acd1043f4234b93124102302b
hosein <hosienlooki0@gmail.com>
2021-01-26 07:41:49 +0330

    service router

--- core/urls.py
+++ core/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from rest_framework import routers
 from .views import (
     TalkViewSet,
-    WorkshopViewSet
+    WorkshopViewSet,
+    UserServicesViewSet,
 )
 
 router = routers.SimpleRouter()
 router.register(r'talks' , TalkViewSet)
 router.register(r'workshop' , WorkshopViewSet)
+router.register('service',UserServicesViewSet)
 
 
 urlpatterns = router.urls
