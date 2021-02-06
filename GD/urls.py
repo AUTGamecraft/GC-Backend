7db47520fe0977a5e73306d5028c84274960304a
hosein <hosienlooki0@gmail.com>
2021-02-06 23:56:26 +0330

    api prefix

--- GD/urls.py
+++ GD/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
     path('api/admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api' , include('core.urls')),
-    path('api' , include('user.urls'))
+    path('api/' , include('core.urls')),
+    path('api/' , include('user.urls'))
 ]
 
 
