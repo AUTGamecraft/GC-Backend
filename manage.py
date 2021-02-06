3ae97ab42fb3fc3c18640f84ed5c71a05d34a43c
hosein <hosienlooki0@gmail.com>
2021-02-06 23:55:54 +0330

    default is development

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GD.settings.production')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GD.settings.developmentgi')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
