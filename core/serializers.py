8eb9f5358c031b229f8768a9569713d1e1172721
aliak <a.akhoundi79@yahoo.com>
2021-02-14 12:10:21 +0330

    fuck

--- core/serializers.py
+++ core/serializers.py
@@ -17,7 +17,7 @@ class PresenterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Presenter
         fields = [
-            'first_name', 'last_name', 'email', 'descriptions', 'linked_in', 'workshops', 'talks','pk'
+            'first_name', 'last_name', 'email', 'descriptions', 'linked_in', 'workshops', 'talks', 'pk'
         ]
         extra_kwargs = {'pk': {'read_only': True}}
 
@@ -45,10 +45,14 @@ class TalksPageSerializer(serializers.ModelSerializer):
 
     remain_capacity = serializers.SerializerMethodField()
 
+    def get_level(self, obj):
+        return obj.get_level_display()
+    level = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Talk
         fields = ['capacity', 'date', 'content', 'title', 'remain_capacity',
-                  'participant_count', 'pk',  'cost', 'presenters' , 'level']
+                  'participant_count', 'pk',  'cost', 'presenters', 'level']
         extra_kwargs = {'pk': {'read_only': True},
                         'remain_capacity': {'read_only': True}}
 
@@ -57,17 +61,16 @@ class WorkshopPageSerializer(serializers.ModelSerializer):
     def get_remain_capacity(self, obj):
         return obj.get_remain_capacity()
 
-    def get_level(self,obj):
+    def get_level(self, obj):
         return obj.get_level_display()
-    level=serializers.SerializerMethodField(read_only=True)
-    level_type=serializers.CharField( max_length=2,write_only=True)
+    level = serializers.SerializerMethodField(read_only=True)
 
     remain_capacity = serializers.SerializerMethodField()
 
     class Meta:
         model = Workshop
         fields = ['capacity', 'date', 'content', 'title', 'remain_capacity',
-                  'participant_count', 'presenters', 'pk', 'cost' , 'level']
+                  'participant_count', 'presenters', 'pk', 'cost', 'level']
         extra_kwargs = {'pk': {'read_only': True},
                         'remain_capacity': {'read_only': True}}
 
