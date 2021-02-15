81fa68cb2066342b4b5f9df7ecd30707ab9aeb32
aliak <a.akhoundi79@yahoo.com>
2021-02-15 04:45:40 +0330

    activation templates updated

--- tasks/emails.py
+++ tasks/emails.py
@@ -8,39 +8,42 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 from django.utils.html import strip_tags
 
+
 def send_email(user):
     context = {
-        'first_name':user['first_name'],
-        'uid':urlsafe_base64_encode(force_bytes(user['pk']))
+        'first_name': user['first_name'],
+        'uid': urlsafe_base64_encode(force_bytes(user['pk'])),
+        'base_url':settings.BASE_URL
     }
 
     email_subject = 'Activation'
-    html_message = render_to_string('activation_message.html',context)
-    plain_message = strip_tags(html_message)
+    html_message = render_to_string('activation_message.html', context)
+    plain_message = render_to_string('activation_message.txt', context)
 
     msg = EmailMultiAlternatives(
         subject=email_subject,
         body=plain_message,
         from_email=settings.DEFAULT_FROM_EMAIL,
-        to=[user['email'],],
+        to=[user['email'], ],
     )
-    msg.attach_alternative(html_message,'text/html')
+    msg.attach_alternative(html_message, 'text/html')
     msg.content_subtype = 'html'
     msg.mixed_subtype = 'related'
     msg.send()
-    return {'success':True}
+    return {'success': True}
+
 
 def send_team_request(team_data):
     context = team_data
     context['tid'] = urlsafe_base64_encode(force_bytes(team_data['tid']))
     context['mid'] = urlsafe_base64_encode(force_bytes(team_data['mid']))
     email_subject = 'Team Request'
-    email_body = render_to_string('team_request_message.txt' , context)
+    email_body = render_to_string('team_request_message.txt', context)
     email = EmailMessage(
         email_subject,
         email_body,
         settings.DEFAULT_FROM_EMAIL,
-        [context['email'],],
+        [context['email'], ],
     )
     email.send(fail_silently=False)
-    return {'success':True}
+    return {'success': True}
