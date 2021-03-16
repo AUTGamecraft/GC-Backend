ecfa3b704ad3bfa36f3b7e179f65e859b3644489
aliak <a.akhoundi79@yahoo.com>
2021-03-16 17:06:19 +0330

    team deletion is ready for admin

--- user/signals.py
+++ user/signals.py
@@ -1,13 +1,12 @@
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
-from .models import Team
+from .models import Team 
+from django.contrib.auth import get_user_model
+
 
 
 
 @receiver(pre_delete , sender=Team)
 def handle_team_delete(sender , instance,**kwargs):
-    mems = instance.members
-    for mem in mems:
-        mem.team_role = 'NO'
-        mem.save()
+    get_user_model().objects.filter(team=instance).update(team_role='NO')
   
