eff36078f32cdd8025a6ebbdb3b2c64d8566b84a
AlirezaZare <azare242@gmail.com>
2023-10-18 10:09:55 +0330

    payping wtf are you doin with us

--- core/payping.py
+++ core/payping.py
@@ -3,43 +3,19 @@ from GD.settings.base import PAYPING_AUTH
 import  json
 
 PAYPING_ERRORCODES = {
-    1: "CanceledByUser",
-    2: "WrongCardPassword",
-    3: "UserDoesnotEnterCVV2OrExpireDate",
-    4: "OutOfBalance",
-    5: "ExpireDateError",
-    6: "CardIsSuspended",
-    7: "PaymentNotFound",
-    8: "BankSuspendedPayment",
-    9: "AmountError",
-    10: "WrongCardNumber",
-    11: "ConnectionError",
-    12: "BankInternalError",
-    15: "PaymentVerified",
-    18: "HosTDoesnotApproved",
-    19: "HosTDoesnotApproved",
-    25: "ServicePermanentlyUnavailable",
-    26: "PaymentCodeError",
-    27: "HosTDoesnotApproved",
-    28: "VPNError",
-    29: "ConnectionError",
-    31: "PaymentVerificationError",
-    38: "AddressError",
-    39: "PaymentFaild",
-    44: "Wrong_refid",
-    46: "TokenError",
-    47: "WrongAmount",
-    48: "ShaparakError",
-    49: "FatalError"
+    
 }
 
 PAYPING_STATUS_OK = 200
+PAYPING_STATUS_201 = 201
+PAYPING_STATUS_202 = 202
 PAYPING_STATUS_CLIENT_ERROR = 400
 PAYPING_STATUS_SERVER_ERROR = 500
 PAYPING_STATUS_503 = 503
 PAYPING_STATUS_AUTH_ERROR = 401
 PAYPING_STATUS_403 = 403
 PAYPING_STATUS_404 = 404
+PAYPING_STATUS_500 = 500
 
 
 
@@ -49,10 +25,10 @@ PAYPING_STATUS_404 = 404
 PayPing_PAYMENT_DESCRIPTION='register workshops or talks'
 PayPing_CALL_BACK='https://gamecraft.ce.aut.ac.ir/api/v2/service/verify/'
 
-PayPing_URL="https://api.payping.ir/v2/pay"
-PayPing_URL_VERIFY="https://api.payping.ir/v2/pay/verify"
+PayPing_URL="https://zify.ir/api/order/v2/create"
+PayPing_URL_VERIFY="https://zify.ir/api/order/v2/verify"
 def PayPingPeymentLinkGenerator(code):
-    return f'https://api.payping.ir/v2/pay/gotoipg/{code}'
+    return f'https://zify.ir/order/accept/{code}'
 class PayPingRequest:
     def __init__(self):
         self.__headers = {
@@ -62,22 +38,42 @@ class PayPingRequest:
 
     def create_payment(self, order_id, amount, name, phone, mail, desc, callback):
         body = {
-            "amount": amount,
-            "payerIdentity": mail,
-            "payerName": name,
-            "description": desc,
-            "returnUrl": callback,
-            "clientRefId": order_id
+            "payer" : {
+                "first_name": "",
+                "last_name": name,
+                "phone": phone,
+                "email": mail,
+                "state": "",
+                "city": "",
+                "address_1": "",
+                "address_2": "",
+
+            },
+            "products" :[
+                {
+                "code": "000",
+                "sellQuantity": 1,
+                "title": "GAME CRAFT PAY",
+                "amount": amount,
+                "unlimited": True,
+                "quantity": 100,
+                "description": PayPing_PAYMENT_DESCRIPTION
+                }
+            ],
+            "returnUrl": PayPing_CALL_BACK,
+            "clientRefId": order_id,
+            "shipping_total": 0,
+            "off_total": 0,
+            "tax_total": 0
            }
         response=requests.request(method='POST',headers=self.__headers,url=PayPing_URL,data=json.dumps(body))
         json_response=json.loads(response.text)
         json_response['status']=response.status_code
         return json_response
         
-    def verify_payment(self,amount ,payment_id):
+    def verify_payment(self, code):
         body = {
-            "refId": payment_id,
-            "amount": amount
+            "order": code
         }
         response = requests.request(method='POST', headers=self.__headers, url=PayPing_URL_VERIFY, data=json.dumps(body))
         json_response = json.loads(response.text)
