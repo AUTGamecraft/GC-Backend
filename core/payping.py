486cf9934e88cfda07f7558cc4e3e92d966f5326
AlirezaYousefpour <93353496+AlirezaYousefpourM@users.noreply.github.com>
2024-09-10 22:29:36 +0330

    Update payping.py

--- core/payping.py
+++ core/payping.py
@@ -75,7 +75,7 @@ class PayPingRequest:
             # "tax_total": 0
         }
         response = requests.request(method='POST', headers=self.__headers, url=PayPing_URL, data=json.dumps(body))
-
+        print(response.text)
         json_response = json.loads(response.text)
         if 'status' not in json_response:
             json_response['status'] = response.status_code
